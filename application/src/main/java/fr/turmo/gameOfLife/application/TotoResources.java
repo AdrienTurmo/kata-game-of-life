21bd16f63434a7fb7daffc2628b859a7b3f41306
AdrienTurmo <adrien.turmo@gmail.com>
2021-01-11 15:41:11 +0100

    Front can call back

--- application/src/main/java/fr/turmo/gameOfLife/application/TotoResources.java
+++ application/src/main/java/fr/turmo/gameOfLife/application/TotoResources.java
@@ -1,12 +1,14 @@
 package fr.turmo.gameOfLife.application;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/toto")
 public class TotoResources {
 
-    @GetMapping("/toto")
+    @GetMapping
     public String getToto() {
         return "toto";
     }
--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CallBackendService} from './call-backend.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private backService: CallBackendService) {
+  }
+
   title = 'front';
+  ploup: string;
+
+  ngOnInit(): void {
+    this.backService.toto().subscribe(data => {
+      this.ploup = data;
+    });
+  }
+
+
 }
